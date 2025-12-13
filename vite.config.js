092cf9879cd46dce0abf16815b7bfe7d3989b05c
Successdanesy <successdanesy@gmail.com>
2025-12-13 15:04:58 +0100

    OG image not displaying

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    publicDir: 'public',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
